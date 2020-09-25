df7abb44cfbb66fa2efb96c72363f55cbbe96298
Aibee <iboroinyang01@gmail.com>
2020-09-25 02:23:51 +0100

    Worked on the footer and added mobile nav

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div id="app" >
     <router-view/>
-    <footer class="w-100 bg-dark text-light">This is the Footer</footer>
+    <!-- Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a> -->
+    <footer class="w-100 text-light row justify-content-space-between mx-auto align-items-center">
+      <div class="col pb-2"><span><img src="../src/assets/church-logo.png" class="img-responsive mr-2"> </span>The Redeemed Christian Church of God</div>
+      <div class="col">
+        <div class="row justify-content-space-between socials pt-md-2">
+          <p class="col-sm-1 text-center pr-md-4 mr-2">Connect With Us</p>
+          <img src="../src/assets/facebook.png" class="col-sm-1 mt-md-2" style="max-width: 55px; height: 25px">
+          <img class="col-sm-1 mt-md-2" src="../src/assets/instagram.png"  style="max-width: 55px; height: 25px">
+          <img class="col-sm-1 mt-md-2" src="../src/assets/twitter.png"  style="max-width: 55px; height: 25px">
+          <img class="col-sm-1 mt-md-2 " src="../src/assets/youtube.png"  style="max-width: 55px; height: 25px">
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -12,8 +24,30 @@ export default {
 </script>
 
 <style>
+@import url(https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700);
+body{
+  font-family: Montserrat, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
 footer {
-  position: absolute;
-  bottom: 0;
+   background-image: linear-gradient(to right, #000033, #00001a, #000000);
+   font-size: 10px;
+   height: 9vh;
+}
+footer > div img{
+   max-width: 1.5rem;
+}
+.socials img{
+  cursor: pointer;
+  -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+    filter: grayscale(100%);
+}
+.socials img:hover, .socials img:active{
+    filter: grayscale(0);
+}
+@media only screen and (max-width: 500px){
+     .socials img{
+          max-width: 45px !important;
+          height: 15px !important;
+     }
 }
 </style>
--- src/views/theNav.vue
+++ src/views/theNav.vue
@@ -1,55 +1,182 @@
 <template>
 <div>
-     <nav class="navbar navbar-expand-lg navbar-dark text-light bg-dark">
+     <nav class="navbar navbar-expand-lg navbar-dark text-light" data-aos="flip-left">
   <a class="navbar-brand" href="#">
        <img src="../assets/church-logo.png">
   </a>
-      <p class="d-xs-none d-lg-block">THE REDEEMED <br> CHRISTIAN CHURCH OF GOD</p>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+      <p class="d-xs-none d-lg-block pt-3">THE REDEEMED <br> CHRISTIAN CHURCH OF GOD</p>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="more-mobile"><img @click="popover = true" src="../assets/more.png"></a>
+    <!-- <a href="#menu">Click me</a> -->
   </button>
-  <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+  <div class="collapse navbar-collapse justify-content-end text-light">
     <ul class="navbar-nav">
-      <li class="nav-item">
+      <li class="nav-item"  data-aos="fade-up">
           <router-link to="/" class="nav-link">Home</router-link>
       </li>
-      <li class="nav-item">
-          <router-link to="About"  class="nav-link">About Us</router-link>
+      <li class="nav-item"  data-aos="fade-down">
+          <router-link to="About"  class="nav-link">About</router-link>
       </li>
-      <li class="nav-item">
+      <li class="nav-item" data-aos="fade-right">
            <router-link to="/gallery"  class="nav-link">Gallery</router-link>
       </li>
     </ul>
   </div>
 </nav>
+<!-- Mobile Nav Starts Here -->
+<div v-show="popover" class="menu">
+ <div>
+   <button type="button" class="close" aria-label="Close" @click="popover = false">
+  <img src="../assets/cancel.png">
+</button>
+  <div class="inner-content border text-light justify-content-center row align-items-center">
+    <ul class="navbar-nav">
+      <li class="nav-item py-2">
+          <router-link to="/" class="nav-link">Home</router-link>
+      </li>
+      <li class="nav-item py-2">
+          <router-link to="About"  class="nav-link">About</router-link>
+      </li>
+      <li class="nav-item py-2">
+           <router-link to="/gallery"  class="nav-link">Gallery</router-link>
+      </li>
+    </ul>
+  </div>
+ </div>
+ </div>
 </div>
 </template>
 
 <script>
+
 export default {
   name: 'Navbar',
   data () {
     return {
+      bal: this.scroller,
+      popover: false
     }
+  },
+  methods: {
+
+  },
+  created: {
+  },
+  computed: {
+    scroller: function () {
+      let val = document.documentElement.scrollTop
+      return val
+    }
+  },
+  watch: {
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+:root {
+  --main-bg-color: coral;
+  --menu-bg: #DE4152;
+--menu-color: #ffffff;
+--menu-font-size: 2.5rem;
+--menu-zindex: 1100;
+--hamburger-color: #fff;
+--hamburger-width: 50px;
+--hamburger-height: 50px;
+--hamburger-zindex: $menu-zindex + 10;
+}
+nav{
+     background-image: linear-gradient(to right, #000033, #00001a, #000000);
+}
+.more-mobile >img, button.close > img{
+     max-width: 1.1rem;
+     filter: invert(100);
+}
+.navbar-toggler, .navbar-toggler:active{
+     border: none !important;
+}
 .router-link-exact-active{
      color: #fff !important;
      font-weight: bolder;
 }
-li.nav-item:hover{
+.navbar li.nav-item:hover{
+     transition: all 0.2s ease-out;
      border-bottom: 3px solid #fff !important;
 }
 .navbar-brand > img {
-     max-width: 90px;
+     max-width: 70px;
 }
 .navbar > p{
      font-weight: 500;
-     font-family: "Berlin Sans FB";
+     font-size: clamp(1rem, calc(0.5rem + 1vw), 1.1rem);
+     /* font-family: "Berlin Sans FB"; */
      letter-spacing: 1px;
+}
+
+.menu {
+  z-index: 1000;
+   box-shadow: 0px 6px 8px rgba(19, 19, 19, .7);
+  animation: 2s fadeIn forwards .5s;
+  background-color: #000033;
+   position: absolute;
+   top: 0;
+   right: 0;
+   left: 0;
+  height: 100vh;
+}
+.inner-content{
+  margin-top: 15vh;
+  margin-left: 25vw;
+  width: 50vw;
+  height: 70vh;
+  border: 1px solid #000;
+  border-radius: 3rem;
+}
+.inner-content li.nav-item{
+   animation: 2s fadeIn forwards .5s;
+}
+button.close{
+  right: 3rem;
+  top: 3rem;
+  width: 25px;
+  height: 25px;
+  position: absolute;
+  display: flex;
+  z-index: 1;
+  border-radius: 8px;
+  font-size: 35px;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  opacity: 0;
+  animation: 1s fadeIn ease forwards .5s;
+}
+
+@keyframes grow {
+  100% {
+    height: 90%;
+    width: 90%;
+ }
+}
+
+@keyframes fadeIn {
+100% {
+  opacity: 1;
+ }
+}
+
+@keyframes slideUp {
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+@media only screen and (max-width: 500px){
+     .navbar-brand > img {
+          max-width: 50px;
+     }
 }
 </style>
